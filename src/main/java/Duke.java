43195fe694fd371647cc1f7b837762eea7c4baec
Pratham Dang <pdan0381@uni.sydney.edu.au>
2026-08-31 17:53:09 +0800

    Level-1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Pratbot.\nWhat can I do for you?");
-        System.out.println("Bye! Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(" " + input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(" Bye. Hope to see you again soon!");
     }
 }
