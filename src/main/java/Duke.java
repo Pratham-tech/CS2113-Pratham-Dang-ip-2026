7320512db6a070cac6d4c90cc6a597dc650a5a4e
Pratham Dang <pdan0381@uni.sydney.edu.au>
2026-08-31 18:00:08 +0800

    Level-3 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Pratbot.\nWhat can I do for you?");
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    System.out.println((i + 1) + "." + tasks.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                Task t = tasks.get(taskNumber - 1);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  " + t);
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                Task t = tasks.get(taskNumber - 1);
+                t.unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + t);
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("added: " + input);
             }
             input = sc.nextLine();
