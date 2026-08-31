5f9d5c1f112b67dfeb01964108e5531f3583c1cf
Pratham Dang <pdan0381@uni.sydney.edu.au>
2026-08-31 17:50:35 +0800

    Level-0 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        System.out.println("Hello! I'm Pratbot.\nWhat can I do for you?");
+        System.out.println("Bye! Hope to see you again soon!");
     }
 }
