083db721b9218c15ba39fd7fe38b92e72447ba83
Pratham Dang <pdan0381@uni.sydney.edu.au>
2026-08-31 17:56:44 +0800

    Level-2 completed, I am doing them quickly as I had completed Week 3 iP requirements but had forked the old cs2113 2021 repo, sorry

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Pratbot.\nWhat can I do for you?");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(" " + input);
-            input = scanner.nextLine();
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("added: " + input);
+            }
+            input = sc.nextLine();
         }
-
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
