d7fb77a3b3ae7dd222196e8ce56b10bc5dfc3c23
Pratham Dang <pdan0381@uni.sydney.edu.au>
2026-09-01 12:16:17 +0800

    Level-4 completed

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -20,7 +20,7 @@ public class Duke {
                 int taskNumber = Integer.parseInt(input.substring(5));
                 Task t = tasks.get(taskNumber - 1);
                 t.markAsDone();
-                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + t);
             } else if (input.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(input.substring(7));
@@ -28,12 +28,37 @@ public class Duke {
                 t.unmarkAsDone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("  " + t);
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                Task t = new Todo(description);
+                tasks.add(t);
+                printAddedTask(t, tasks.size());
+            } else if (input.startsWith("deadline ")) {
+                String remainder = input.substring(9);
+                String[] parts = remainder.split(" /by ", 2);
+                Task t = new Deadline(parts[0], parts[1]);
+                tasks.add(t);
+                printAddedTask(t, tasks.size());
+            } else if (input.startsWith("event ")) {
+                String remainder = input.substring(6);
+                String[] fromSplit = remainder.split(" /from ", 2);
+                String[] toSplit = fromSplit[1].split(" /to ", 2);
+                Task t = new Event(fromSplit[0], toSplit[0], toSplit[1]);
+                tasks.add(t);
+                printAddedTask(t, tasks.size());
             } else {
-                tasks.add(new Task(input));
-                System.out.println("added: " + input);
+                Task t = new Todo(input);
+                tasks.add(t);
+                printAddedTask(t, tasks.size());
             }
             input = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void printAddedTask(Task t, int totalTasks) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + t);
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
     }
 }
